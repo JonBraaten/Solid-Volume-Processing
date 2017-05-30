17a4108c35be2e2bed9316acf8ec160b06c81ead
Jon Braaten <jbraaten@andrew.cmu.edu>
2017-05-30 15:26:33 -0400

    Add files via upload

--- Visualize_Stack.m
+++ Visualize_Stack.m
@@ -1,18 +1,19 @@
-
+%Jonathan Braaten
+%Carnegie Mellon University
+%Completed May 30, 2017
+%Visualize_Stack.m
+%---------------------------
+%This script opens and scrolls through a single .tif stack using MATLAB's image viewer to allow for
+%visualization of each image in the .tif stack.
 clear;
 clc;
-filename1='EOL_T8_Mem_Norm_Cropped.tif'
-filename2='EOL_T8_Mem_Norm_Cropped2.tif'
-info=imfinfo(filename1);
-info2=imfinfo(filename2);
-num=numel(info);
-for i=1:num
-    I=imread(filename1,i);
-    I2=imread(filename2,i);
-    figure(1)
-    imshow(imadjust(I));
-    figure(2)
-    imshow(imadjust(I2));
-    pause()
-    i
+filename1='EOL_T8_Mem_Norm_Cropped2.tif'; %reading file
+info=imfinfo(filename1); %getting stack information
+num=numel(info); %finding numer of images in stack
+for i=1:num %iterating through each image in stack
+    I=imread(filename1,i); %reading each image in stack
+    figure(1) %opening new figure
+    imshow(imadjust(I)); %showing each image in stack
+    pause(); %pausing to allow for manual scrolling through stack
+    i %displaying image number in command window
 end
