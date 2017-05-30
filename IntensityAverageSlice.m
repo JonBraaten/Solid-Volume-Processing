0748b7a5c2b5e63643bb72cd67038788e7e78393
Jon Braaten <jbraaten@andrew.cmu.edu>
2017-05-30 15:46:16 -0400

    Add files via upload

--- IntensityAverageSlice.m
+++ IntensityAverageSlice.m
@@ -1,25 +1,24 @@
-function [ INT_AVG,DIST,X ] = IntensityAverageSlice( filename,voxel_size )
+%Jonathan Braaten
+%Carnegie Mellon University
+%Completed May 30, 2017
+%IntensityAverageSlice.m
+%---------------------------
 %IntensityAverageSlice takes the average intensity of each vertical column or "slice"
-%from a 3D tiff stack. Takes a filename string input, and voxel size, and
-%outputs the average intensity per slice array and a distance array
-info=imfinfo(filename);  %importing info from TIFF file
-num_images=numel(info); %number of images in TIFF stack
-for k=1:num_images %for each image in stack
-    I=imread(filename,k); %reading intensisty info from each image in stack
-    [a,b]=size(I); %finding size of each image
-    for i=1:a %for each row element of each image
-        for j=1:b %for each column element of each row of each image
-            INT(i,j,k)=I(i,j); %assigning intensity data into each element (row, column, image)
-        end
+%from a 3D tiff stack. Takes a filename string input, voxel size, and Intensity Range, and
+%outputs the average intensity per slice array ("INT_AVG") and a distance through the
+%volume ("X") as well as the lognormal distribution specified by "DIST" and
+%the intensity range specified by "Y" for plotting.
+function [ INT_AVG,X,Y,DIST ] = IntensityAverageSlice( filename,voxel_size,INT_RANGE ) %setting inputs/outputs
+[INT] = int_assign(filename); %calling int_assign function to write intensity data to a 3-D matrix
+[a,b,c]=size(INT); %calculating the size of the 3-D matrix
+for j=1:b %iterating for each slice of the 3-D matrix (thickness of MEA direction)
+    INT_AVG(j)=mean(nonzeros(INT(:,j,:))); %averaging non-zero terms of column slice
+    [Y,D]=lognorm_dist(nonzeros(squeeze(double(INT(:,j,:)))),INT_RANGE); %log-normal fitting of data in each slice
+    for i=1:length(Y) %iterating through log normal distribution data
+        DIST(j,i)=D(i); %assigning log normal data points to array slots in INT_RANGE
     end
 end
-
-for j=1:b %for each column slice in INT
-    ROW_I=INT(:,j,:); %making 1-D array out of the 2-D matrix INT for each column slice in INT
-    INT_AVG(j)=mean(nonzeros(ROW_I)); %averaging non-zero terms of column slice
-    DIST(:,j)
-end
-X=[0:voxel_size:(length(INT_AVG)-1)*voxel_size]; %distance array - number of voxels times voxel size
+X=[0:voxel_size:(b-1)*voxel_size]; %distance array - number of voxels times voxel size
 %end function
 end
 
